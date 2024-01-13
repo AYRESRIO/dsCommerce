e826fbb536590c6a9e537a3490a53013a18d890a
AYRESRIO <AYRESRIO@HOTMAIL.COM>
2024-01-13 03:12:30 -0300

    User and Order classes created.

---
+++ src/main/java/com/devsuperior/dscommerece/DscommereceApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscommerece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommereceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommereceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dscommerece/DscommereceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscommerece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommereceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
