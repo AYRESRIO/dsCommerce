02370efffe6e258b9491076866d807c6ef1be284
AYRESRIO <AYRESRIO@HOTMAIL.COM>
2024-02-16 17:48:14 -0300

    Fix validation

--- src/main/java/com/devsuperior/dscommerece/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerece/dto/ProductDTO.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 
@@ -12,20 +13,22 @@ import com.devsuperior.dscommerece.entities.Category;
 import com.devsuperior.dscommerece.entities.Product;
 
 public class ProductDTO {
-	
+
 	private Long id;
-	@Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres.")
+	@Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
 	@NotBlank(message = "Campo requerido")
 	private String name;
 
-	@Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres.")
+	@Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
 	@NotBlank(message = "Campo requerido")
 	private String description;
-	@Positive(message = "O preço deve ser positivo.")
+
+	@NotNull(message = "Campo requerido")
+	@Positive(message = "O preço deve ser positivo")
 	private Double price;
 	private String imgUrl;
-	
-	@NotEmpty(message = "Deve ter pelo menos uma categoria.")
+
+	@NotEmpty(message = "Deve ter pelo menos uma categoria")
 	private List<CategoryDTO> categories = new ArrayList<>();
 
 	public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
@@ -42,7 +45,7 @@ public class ProductDTO {
 		description = entity.getDescription();
 		price = entity.getPrice();
 		imgUrl = entity.getImgUrl();
-		for(Category cat : entity.getCategories()) {
+		for (Category cat : entity.getCategories()) {
 			categories.add(new CategoryDTO(cat));
 		}
 
@@ -71,6 +74,5 @@ public class ProductDTO {
 	public List<CategoryDTO> getCategories() {
 		return categories;
 	}
-	
-	
+
 }
